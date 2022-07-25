d7a14d42193b41d8c100965f1ec778442364ccc7
Rushil Sharma <rushils820@gmail.com>
2022-07-25 21:28:45 +0530

    ..

---
+++ heart.py
@@ -0,0 +1,21 @@
+from turtle import*
+speed(0)
+pencolor('red')
+pensize(5)
+fillcolor('pink')
+begin_fill()
+right(60)
+forward(111.65)
+left(120)
+forward(111.65)
+for i in range(200) : 
+    left(1)
+    forward(0.5)
+right(160)
+for i in range(200) : 
+    left(1)
+    forward(0.5)
+
+end_fill()
+mainloop()
+
---
+++ pattern.py
@@ -0,0 +1,8 @@
+from turtle import*
+speed('fastest')
+for i in range(1,100): 
+    for j in range(1,6):
+        left(144)
+        forward(200)
+    left(5)
+mainloop()
