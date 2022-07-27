2f24232a1ffbf3acdc65b09313932be2df5ef09d
Rushil Sharma <rushils820@gmail.com>
2022-07-27 13:22:09 +0530

    ..

--- yon.py
+++ yon.py
@@ -1,13 +1,14 @@
 from turtle import * 
-for i in range(2):
- forward (400) 
- left (90) 
- forward(200)
- left(90)
 c=[(-300,300),
     (0,300),
     (300,300),
     (-150,150),
     (150,150),
     (0,0)]
+penup()
+for i in range(6):
+    goto(c[i])
+    pendown()
+    dot(50)
+    penup()
 mainloop()
